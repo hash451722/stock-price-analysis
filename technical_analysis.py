b49e4812f1c1be544299ddf205719a2c29e23e04
hash451722 <93071591+hash451722@users.noreply.github.com>
2023-03-07 03:05:47 +0900

    update

--- technical_analysis.py
+++ technical_analysis.py
@@ -1,5 +1,8 @@
 import pathlib
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
 
 
 class TechnicalAnalysis():
@@ -12,7 +15,33 @@ class TechnicalAnalysis():
         return df
 
 
+    def macd(self, df:pd.DataFrame, short_span:int=12, long_span:int=26, signal_span:int=9) -> pd.DataFrame:
+        ''' MACD : Moving Average Convergence Divergence '''
+        short_ema = df['Close'].ewm(span=short_span, adjust=False).mean()
+        long_ema = df['Close'].ewm(span=long_span, adjust=False).mean()
+        macd = short_ema - long_ema
+        signal_line = macd.ewm(span=signal_span, adjust=False).mean()
+        histgram = macd - signal_line
 
+        df['MACD'] = macd
+        df['SignalLine'] = signal_line
+        df['Histgram'] = histgram
+        return df
+    
+    
+    def rsi(self, df:pd.DataFrame, span:int=14):
+        ''' RSI : Relative Strength Index '''
+        up = df["Close"].diff().clip(lower=0).rolling(span).mean()
+        down = df["Close"].diff().clip(upper=0).rolling(span).mean()
+        rsi = 100 - 100 / ( 1 - up / down )
+        df["RSI"] = rsi
+        return df
+
+
+    def plot(self, df:pd.DataFrame):
+        # df[['MACD', 'SignalLine']].plot(figsize=(10, 5))
+        df[['RSI']].plot(figsize=(10, 5))
+        plt.show()
 
 
 
@@ -21,4 +50,11 @@ if __name__ == '__main__':
     path_csv = path_current_dir.joinpath("data", "101.csv")
 
     ta = TechnicalAnalysis(path_csv)
+
+    ta.df = ta.macd(ta.df)
+    ta.df = ta.rsi(ta.df, 14)
+
+
     print(ta.df)
+
+    ta.plot(ta.df.loc["2022":"2023"])
